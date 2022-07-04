4a1fd8d07844ad1edfc7c54157095114300847c8
DayanchMuhammetgeldiyev <muhammetgeldiyevdayanc@gmail.com>
2022-07-04 17:49:32 +0300

    form-go

---
+++ Form/form.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", processor)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func processor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// get form values
+	firster := r.FormValue("firster")
+	laster := r.FormValue("laster")
+
+	// process form values
+
+	// display form values
+	tpl.ExecuteTemplate(w, "processor.gohtml", firster, laster)
+}
